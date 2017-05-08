e3a3469e4c7013e3a8f44b29ecf62bcf80c7dc8f
afghl <airport.0930@gmail.com>
2017-05-08 08:44:39 +0800

    add rpc exporter and rpc importer

---
+++ java/myrpcdemo/RpcImporter.java
@@ -0,0 +1,10 @@
+package myrpcdemo;
+
+import java.lang.reflect.Proxy;
+import java.net.InetSocketAddress;
+
+public class RpcImporter<S> {
+    public S importer(final Class<?> serviceClass, final InetSocketAddress addr) {
+        
+    }
+}
